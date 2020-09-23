8f418e4fbe9488f65183e421c47dab6f401dc738
Haiyang Hong <hong961127@gmail.com>
2020-09-23 10:43:44 -0700

    Setup next app with apollo client, material ui

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,22 @@
-import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+
+import Navbar from "../components/Navbar";
+import { useApollo } from "../lib/apolloClient";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const client = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
---
+++ client/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0099e5",
+    },
+    secondary: {
+      main: "#ff4c4c",
+    },
+  },
+});
+
+export default theme;
