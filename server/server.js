43bc8058e0a78af8d2d92bf8d9a4f36ac57537c8
Haiyang Hong <hong961127@gmail.com>
2020-09-23 06:51:55 -0700

    Setup apollo-server-express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 require("dotenv").config({ path: "./config/config.env" });
+
 const express = require("express");
+const http = require("http");
+const { ApolloServer } = require("apollo-server-express");
+
 const morgan = require("morgan");
 const cors = require("cors");
 
@@ -12,5 +16,42 @@ if (process.env.NODE_ENV === "development") {
   app.use(cors(process.env.CLIENT_URL));
 }
 
+// Query, Mutation, Subscription types
+const typeDefs = `
+  type Query {
+    totalPosts: Int!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    totalPosts: () => 42,
+  },
+};
+
+// Create Apollo Server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// applyMiddleware connects ApolloServer to Express app
+apolloServer.applyMiddleware({
+  app,
+});
+
+// Create a server for rest endpoints
+const httpServer = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server up on port ${port}...`));
+app.listen(port, () => {
+  console.log(`Server is up at http://localhost:${port}...`);
+  console.log(
+    `GraphQL server is up at http://localhost:${port}${apolloServer.graphqlPath}...`
+  );
+});
