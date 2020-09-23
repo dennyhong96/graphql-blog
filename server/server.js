1166fbf7a30ad7c06ccba9d563b36056870feffe
Haiyang Hong <hong961127@gmail.com>
2020-09-23 06:24:22 -0700

    Setup middlewares

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors(process.env.CLIENT_URL));
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server up on port ${port}...`));
