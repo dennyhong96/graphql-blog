62d75f6a712cb53f283b6ea2069e9152a7315587
Haiyang Hong <hong961127@gmail.com>
2020-09-23 09:54:18 -0700

    add next.js

--- server/graphql/resolvers/post.js
+++ server/graphql/resolvers/post.js
@@ -1,7 +1,41 @@
-const totalPosts = () => 42;
+const POSTS = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, accusantium!",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, accusantium!",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, accusantium!",
+  },
+];
+
+const totalPosts = () => POSTS.length;
+
+const listPosts = () => POSTS;
+
+const createPost = (_, { input }, ctx) => {
+  const id = POSTS.map((p) => p.id).sort((a, b) => a - b)[POSTS.length - 1] + 1;
+  const newPost = { id, ...input };
+  POSTS.push(newPost);
+  return newPost;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    listPosts,
+  },
+  Mutation: {
+    createPost,
   },
 };
--- server/graphql/typeDefs/post.js
+++ server/graphql/typeDefs/post.js
@@ -1,7 +1,25 @@
 const { gql } = require("apollo-server-express");
 
 module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+  }
+
+  # input types
+  input CreatePostInput {
+    title: String!
+    description: String!
+  }
+
   type Query {
     totalPosts: Int!
+    listPosts: [Post!]!
+  }
+
+  # Mutation
+  type Mutation {
+    createPost(input: CreatePostInput!): Post!
   }
 `;
