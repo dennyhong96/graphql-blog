02b32ad33ddad94ad5def9d14aedcc1ca89d2cd0
Haiyang Hong <hong961127@gmail.com>
2020-09-26 17:40:58 -0700

    Add reset password popup

---
+++ client/src/components/ui/Dialog.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const ResetPasswordDialog = ({
+  children,
+  title,
+  open,
+  handleClose,
+  confirmButtonText,
+  handleConfirm,
+  loading,
+  disabled,
+}) => {
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary" disabled={loading}>
+          Cancel
+        </Button>
+        <Button
+          onClick={handleConfirm}
+          color="primary"
+          variant="contained"
+          autoFocus
+          disabled={disabled || loading}
+        >
+          {loading ? <CircularProgress size={24} /> : confirmButtonText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ResetPasswordDialog;
--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,5 +1,12 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext, useState } from "react";
+import { toast } from "react-toastify";
 
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+import { auth } from "../../services/firebase.js";
+import Dialog from "../../components/ui/Dialog";
 import { AuthContext } from "../../context/authContext";
 
 const Profile = () => {
@@ -7,7 +14,70 @@ const Profile = () => {
     state: { user },
   } = useContext(AuthContext);
 
-  return <h1>Welcome back, {user?.email.split("@")[0]}!</h1>;
+  const [open, setOpen] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleResetPassword = async () => {
+    if (password !== passwordConfirm) {
+      return toast.error("Passwords do not match.");
+    }
+    setLoading(true);
+    try {
+      setLoading(false);
+      await auth.currentUser.updatePassword(password);
+      toast.success("Password successfully reset.");
+      setPassword("");
+      setPasswordConfirm("");
+      setOpen(false);
+    } catch (error) {
+      console.error("[RESET PASSWORD ERROR]", error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Fragment>
+      <Typography variant="h6" component="h1">
+        Welcome back, {user?.email.split("@")[0]}!
+      </Typography>
+      <Button variant="contained" onClick={setOpen.bind(this, true)}>
+        Reset Password
+      </Button>
+      <Dialog
+        title="Reset your password"
+        open={open}
+        handleClose={setOpen.bind(this, false)}
+        confirmButtonText="Reset"
+        loading={loading}
+        handleConfirm={handleResetPassword}
+        disabled={!(password && passwordConfirm)}
+      >
+        <TextField
+          id="reset-password"
+          label="Enter your new password"
+          fullWidth
+          value={password}
+          onChange={(evt) => setPassword(evt.target.value)}
+          style={{ marginBottom: "0.25rem" }}
+          inputProps={{ type: "password" }}
+          disabled={loading}
+        />
+        <TextField
+          id="reset-password-confirm"
+          label="Confirm your new password"
+          fullWidth
+          value={passwordConfirm}
+          onChange={(evt) => setPasswordConfirm(evt.target.value)}
+          style={{ marginBottom: "1rem" }}
+          inputProps={{ type: "password" }}
+          disabled={loading}
+        />
+      </Dialog>
+    </Fragment>
+  );
 };
 
 export default Profile;
