44492f04e26ec005bd0981fff536efc60ed921b4
Haiyang Hong <hong961127@gmail.com>
2020-09-30 22:46:11 -0700

    Configure subscription for apolloServer

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config({ path: "./config/config.env" });
 const path = require("path");
-// const http = require("http");
+const http = require("http");
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer, PubSub } = require("apollo-server-express");
 const {
   makeExecutableSchema,
   mergeTypeDefs,
@@ -16,6 +16,7 @@ const connectDB = require("./config/db");
 const imageRouter = require("./routes/image");
 
 const app = express();
+const pubSub = new PubSub(); // PubSub instance for subscription
 connectDB();
 
 // Middlewares
@@ -39,19 +40,23 @@ const resolvers = mergeResolvers(
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req, res }) => ({ req, res }),
+  context: ({ req, res, pubSub }) => ({ req, res, pubSub }),
 });
 
 // applyMiddleware connects ApolloServer to Express app
 apolloServer.applyMiddleware({ app });
 
-// Create a server for rest endpoints
-// const httpServer = http.createServer(app);
+// Create a http server for pubSub
+const httpServer = http.createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is up at http://localhost:${port}...`);
   console.log(
     `GraphQL server is up at http://localhost:${port}${apolloServer.graphqlPath}...`
+  );
+  console.log(
+    `GraphQL subscriptions is up at http://localhost:${port}${apolloServer.subscriptionsPath}...`
   );
 });
